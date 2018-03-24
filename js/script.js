699f17dcc6d32b37303ca3b78e51a41e77562baf
Valentyn Baralyuk <valentyn.baralyuk@gmail.com>
2018-03-24 03:36:54 +0200

    fixed carousel, added sample footer and pagination (change later)

--- js/script.js
+++ js/script.js
@@ -10,17 +10,20 @@ function currentSlide(index) {
     if (slideIndex > slides.length) {index = 1}
     else if(slideIndex < 1){index = slides.length}
     for (i = 0; i < slides.length; i++) {   
+        slides[i].style.visibility = "hidden";
         slides[i].style.opacity = 0;  
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+    slides[slideIndex - 1].style.visibility = "visible"  
     slides[slideIndex - 1].style.opacity = 1;  
     dots[slideIndex - 1].className += " active";
 }
 
 function showSlides() {
     for (i = 0; i < slides.length; i++) {
+        slides[i].style.visibility = "hidden";  
         slides[i].style.opacity = 0;  
     }
     slideIndex++;
@@ -28,7 +31,8 @@ function showSlides() {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.opacity = 1;  
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.visibility = "visible";  
+    slides[slideIndex - 1].style.opacity = 1;  
+    dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 7000);
 }
